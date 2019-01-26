8e33f081f9ca3b254befbe9643301bb9640829cf
kimseasok <basicblogtalk@gmail.com>
2019-01-25 21:57:15 -0500

    M4V6: Hide and show element with ngif

--- src/app/events/event-list.component.ts
+++ src/app/events/event-list.component.ts
@@ -101,11 +101,7 @@ export class EventsListComponent {
       time: '9:00 am',
       price: 950.0,
       imageUrl: '/assets/images/ng-nl.png',
-      location: {
-        address: 'The NG-NL Convention Center & Scuba Shop',
-        city: 'Amsterdam',
-        country: 'Netherlands'
-      },
+      onlineOnly: 'https://basicblogtalk.com',
       sessions: [
         {
           id: 1,
--- src/app/events/event-thumnail.component.ts
+++ src/app/events/event-thumnail.component.ts
@@ -3,14 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'event-thumnail',
   template: `
-    <div class="well hoverwell thumnail">
+    <div class="well hoverwell thumnail" *ngIf="event">
       <h2>{{ event.name }}</h2>
       <div>Date: {{ event.date }}</div>
       <div>Time: {{ event.time }}</div>
       <div>Price: \${{ event.price }}</div>
-      <div>
-        <span>Location: {{ event.location.address }}</span> <span>&nbsp;</span>
+      <div *ngIf="event.location">
+        <span>Location: {{ event.location.address }}</span>
+        <span>&nbsp;</span>
         <span>{{ event.location.city }}, {{ event.location.country }}</span>
+      </div>
+      <div *ngIf="event.onlineOnly">
+        <span>Online only: {{event.onlineOnly}}</span>
       </div>
     </div>
   `,
