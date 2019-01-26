1629f099521b5650243e75242b4e2e9643ca02f1
kimseasok <basicblogtalk@gmail.com>
2019-01-25 22:17:29 -0500

    M4V9: Use ngswtich to add time status

--- src/app/events/event-thumnail.component.ts
+++ src/app/events/event-thumnail.component.ts
@@ -6,11 +6,15 @@ import { Component, Input } from '@angular/core';
     <div class="well hoverwell thumnail" *ngIf="event">
       <h2>{{ event.name }}</h2>
       <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
+      <div [ngSwitch]="event?.time">
+      Time: {{ event.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+        <span *ngSwitchDefault>(Normal start)</span>
+      </div>
       <div>Price: \${{ event.price }}</div>
       <div *ngIf="event.location">
         <span>Location: {{ event.location.address }}</span>
-        <span>&nbsp;</span>
         <span>{{ event.location.city }}, {{ event.location.country }}</span>
       </div>
       <div *ngIf="event.onlineOnly">
