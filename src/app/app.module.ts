5c77dff55a740bfaa66b3b006a1e996244409613
kimseasok <basicblogtalk@gmail.com>
2019-01-25 23:02:38 -0500

    Add injectable service for event list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { EventAppComponent } from './event-app.component';
 import { EventsListComponent } from './events/event-list.component';
 import { EventsThumnailComponent } from './events/event-thumnail.component';
 import { NavBarComponent } from './nav/nav-bar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [EventAppComponent, EventsListComponent, EventsThumnailComponent, NavBarComponent],
+  providers: [EventService],
   bootstrap: [EventAppComponent]
 })
 export class AppModule {}
