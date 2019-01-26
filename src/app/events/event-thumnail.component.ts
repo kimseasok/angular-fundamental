4598734b538020d62a91ebd163f2c5fc68b50f4c
kimseasok <basicblogtalk@gmail.com>
2019-01-25 22:33:45 -0500

    Add class using class binding and ngclass

--- src/app/events/event-thumnail.component.ts
+++ src/app/events/event-thumnail.component.ts
@@ -6,24 +6,27 @@ import { Component, Input } from '@angular/core';
     <div class="well hoverwell thumnail" *ngIf="event">
       <h2>{{ event.name }}</h2>
       <div>Date: {{ event.date }}</div>
-      <div [ngSwitch]="event?.time">
-      Time: {{ event.time }}
+      <div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">
+        Time: {{ event.time }}
         <span *ngSwitchCase="'8:00 am'">(Early start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late start)</span>
         <span *ngSwitchDefault>(Normal start)</span>
       </div>
       <div>Price: \${{ event.price }}</div>
-      <div *ngIf="event.location">
+      <div *ngIf="event.location" [ngClass]="{green: event?.location.country === 'USA', 'bold-text': event?.location.country === 'USA'}">
         <span>Location: {{ event.location.address }}</span>
         <span>{{ event.location.city }}, {{ event.location.country }}</span>
       </div>
       <div *ngIf="event.onlineOnly">
-        <span>Online only: {{event.onlineOnly}}</span>
+        <span>Online only: {{ event.onlineOnly }}</span>
       </div>
     </div>
   `,
   styles: [
     `
+      .green {
+        color: #003300 !important;
+      }
       .thumnail {
         min-height: 210px;
       }
