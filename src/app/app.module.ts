bf34e8684f4c19bd243619a5aa038dee159860dd
kimseasok <basicblogtalk@gmail.com>
2019-01-25 23:47:36 -0500

    Add toastr third-party service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { EventsListComponent } from './events/event-list.component';
 import { EventsThumnailComponent } from './events/event-thumnail.component';
 import { NavBarComponent } from './nav/nav-bar.component';
 import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toast.service';
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [EventAppComponent, EventsListComponent, EventsThumnailComponent, NavBarComponent],
-  providers: [EventService],
+  providers: [EventService, ToastrService],
   bootstrap: [EventAppComponent]
 })
 export class AppModule {}
--- src/app/events/event-list.component.ts
+++ src/app/events/event-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toast.service';
 
 @Component({
   selector: 'event-list',
@@ -9,7 +10,7 @@ import { EventService } from './shared/event.service';
       <hr />
       <div class="row">
         <div class="col-md-5" *ngFor="let event of events">
-          <event-thumnail [event]="event"></event-thumnail>
+          <event-thumnail (click)="handleThumnailClick(event.name)" [event]="event"></event-thumnail>
         </div>
       </div>
     </div>
@@ -17,9 +18,13 @@ import { EventService } from './shared/event.service';
 })
 export class EventsListComponent implements OnInit {
   events: any[];
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private toastr: ToastrService) {}
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumnailClick(eventName) {
+    this.toastr.success(eventName, 'clicked');
   }
 }
