930c512796aa0c3336a6efffb8abae62a8fae466
kimseasok <basicblogtalk@gmail.com>
2019-01-27 22:04:58 -0500

    Profile form validator

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,24 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+
+      .error input {
+        background-color: #e3c3c6;
+      }
+
+      .error :: -webkit-input-placeholder {
+        color: #999;
+      }
+
+      .error :: -moz-placeholder {
+        color: #999;
+      }
+      .error: -moz-placeholder {
+        color: #999;
+      }
+      .error :ms-input-placeholder {
+        color: #999;
+      }
+    `
+  ]
 })
 export class ProfileComponent implements OnInit {
   constructor(private auth: AuthService, private router: Router) {}
+
   profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
+
   ngOnInit() {
-    const firstName = new FormControl(this.auth.currentUser.firstName);
-    const lastName = new FormControl(this.auth.currentUser.lastName);
+    this.firstName = new FormControl(
+      this.auth.currentUser.firstName,
+      Validators.required
+    );
+    this.lastName = new FormControl(
+      this.auth.currentUser.lastName,
+      Validators.required
+    );
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     });
   }
 
+  validFirstName() {
+    return this.firstName.valid || this.firstName.untouched;
+  }
+
+  validLastName() {
+    return this.lastName.valid || this.lastName.untouched;
+  }
+
   saveProfile(formValues) {
+    if (this.profileForm.invalid) {
+      return;
+    }
     this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
     this.router.navigate(['events']);
   }
