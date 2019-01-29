7732edf0443b322248ac87724d00ce4a0c15afec
kimseasok <basicblogtalk@gmail.com>
2019-01-28 22:11:12 -0500

    Content projection: Multiple slots projection (class selecting)

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,8 +4,8 @@ import { Component, Input } from '@angular/core';
   selector: 'collapsible-well',
   template: `
     <div (click)="toggleContent()" class="well pointable">
-      <h4 class="well-title">{{ title }}</h4>
-      <ng-content *ngIf="visible"></ng-content>
+      <h4><ng-content select=".session-title"></ng-content></h4>
+      <ng-content select=".session-body" *ngIf="visible"></ng-content>
     </div>
   `
 })
