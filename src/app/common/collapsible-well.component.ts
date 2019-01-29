c6f921a75b96aa573cf1f80beb26cd3e89282754
kimseasok <basicblogtalk@gmail.com>
2019-01-28 22:14:23 -0500

    Content projection: multiple slot (attribute selection)

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,8 +4,8 @@ import { Component, Input } from '@angular/core';
   selector: 'collapsible-well',
   template: `
     <div (click)="toggleContent()" class="well pointable">
-      <h4><ng-content select=".session-title"></ng-content></h4>
-      <ng-content select=".session-body" *ngIf="visible"></ng-content>
+      <h4><ng-content select="[well-title]"></ng-content></h4>
+      <ng-content select="[well-body]" *ngIf="visible"></ng-content>
     </div>
   `
 })
