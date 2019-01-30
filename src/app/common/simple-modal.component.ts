602a8e26e7e8e2219dd10284bebb40b00dff0167
kimseasok <basicblogtalk@gmail.com>
2019-01-29 22:23:20 -0500

    close modal dialog using VievChild

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,9 +1,18 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  inject,
+  Inject
+} from '@angular/core';
+import { $ } from 'protractor';
+import { JQUERY_TOKEN } from './jquery.service';
 
 @Component({
   selector: 'simple-modal',
   template: `
-    <div id="simple-modal" class="modal fade" index="-a">
+    <div id="simple-modal" class="modal fade" index="-a" #modalContainer>
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -12,7 +21,9 @@ import { Component, Input } from '@angular/core';
             </button>
             <h4 class="modal-title">{{ title }}</h4>
           </div>
-          <div class="modal-body"><ng-content></ng-content></div>
+          <div class="modal-body" (click)="closeModal()">
+            <ng-content></ng-content>
+          </div>
         </div>
       </div>
     </div>
@@ -27,4 +38,11 @@ import { Component, Input } from '@angular/core';
 })
 export class SimpleModalComponent {
   @Input() title: string;
+  @ViewChild('modalContainer') containerEl: ElementRef;
+
+  constructor(@Inject(JQUERY_TOKEN) private $: any) {}
+
+  closeModal() {
+    this.$(this.containerEl.nativeElement).modal('hide');
+  }
 }
